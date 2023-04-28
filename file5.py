c54b804001ef55aa80df4c0e9b1ac092c52e7658
Pavitra2005 <upavitra2005@gmail.com>
2023-04-28 17:39:44 +0530

    this is python

---
+++ file5.py
@@ -0,0 +1,10 @@
+class IOString():
+	def _init_(self):
+		self.str1=""
+	def get_string(self):
+		self.str1=input()
+	def print_string(self):
+		print(self.str.upper())
+str1=IOString()
+str1.get_String()
+str1.print_string()
---
+++ file6.py
@@ -0,0 +1,8 @@
+import math
+number=input("provide D")
+numbers=numbers.split(',')
+result_list=[]
+for D in numbers:
+	Q=round(mat.sqrt(2*50*int(D)/30))
+	result_list.append(Q)
+print(result_list)
